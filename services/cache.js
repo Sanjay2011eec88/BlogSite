e4a8b8cc47633e4c83e38909af6fec3b7b52a0c2
VarmaSANJAY <sanjay.varma@relevacelab.com>
2018-06-28 20:39:33 +0530

    Nested Hasshes

--- services/cache.js
+++ services/cache.js
@@ -5,12 +5,15 @@ const util = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
 //This function will be called when we need to run cache function otherwise, the caching will be occur
-mongoose.Query.prototype.cache = function(){
+mongoose.Query.prototype.cache = function(options = {}){
     this.useCache = true;
+    //Using this to create a nested hash
+    //If key is not passed we handle that condition by entering empty string
+    this.hashKey = JSON.stringify(options.key || "");
     return this;
 }
 
@@ -27,7 +30,7 @@ mongoose.Query.prototype.exec = async function(){
         })
     )
     //See if we have value for 'key' in redis
-    const cacheValue = await client.get(key);
+    const cacheValue = await client.hget(this.hashKey, key);
 
     //If we do, return that
     if(cacheValue){
@@ -44,7 +47,10 @@ mongoose.Query.prototype.exec = async function(){
     //Otherwise, issue the query and store the result in redis
     const result =  await exec.apply(this,arguments);
     
-    client.set(key, JSON.stringify(result));
+    client.hset(this.hashKey, key, JSON.stringify(result));
+    
+    //TO set expiration for a cahe value
+    //client.set(key, JSON.stringify(result), 'EX', 10);
     
     return result;
 }
