e0bcb45cbfb727f71c908d6e0869722d0016eecb
VarmaSANJAY <sanjay.varma@relevacelab.com>
2018-06-28 17:43:01 +0530

    Cache Toggeable

--- services/cache.js
+++ services/cache.js
@@ -1,9 +1,50 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
 
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function(){
-    console.log('I am about to run');
+//This function will be called when we need to run cache function otherwise, the caching will be occur
+mongoose.Query.prototype.cache = function(){
+    this.useCache = true;
+    return this;
+}
 
-    return exec.apply(this, arguments);
+mongoose.Query.prototype.exec = async function(){
+
+    if(!this.useCache){
+        return exec.apply(this,arguments);
+    }
+
+    //Object.assign is used to create a copy of object
+    const key = JSON.stringify(
+            Object.assign({}, this.getQuery(),{
+            collection: this.mongooseCollection.name
+        })
+    )
+    //See if we have value for 'key' in redis
+    const cacheValue = await client.get(key);
+
+    //If we do, return that
+    if(cacheValue){
+        const doc = JSON.parse(cacheValue);
+
+        //This doc will return either array of objects or single object
+        //We need to convert it to mongoose object and then send.
+
+        return Array.isArray(doc)
+        ? doc.map(d => new this.model(d))
+        : new this.model(doc);
+    }
+
+    //Otherwise, issue the query and store the result in redis
+    const result =  await exec.apply(this,arguments);
+    
+    client.set(key, JSON.stringify(result));
+    
+    return result;
 }
